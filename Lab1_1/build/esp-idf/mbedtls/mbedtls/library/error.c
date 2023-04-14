22a7f83efb7fd86751e8b064d7b4f3d96aa81a4b
chiara-knicker <chiara.knicker@gmail.com>
2023-04-14 01:10:59 -0700

    first commit

---
+++ Lab1_1/build/esp-idf/mbedtls/mbedtls/library/error.c
@@ -0,0 +1,1 @@
+/home/chiar/CSE121_Labs/esp-idf/components/mbedtls/mbedtls/library/error.c
---
+++ Lab1_1/build/esp-idf/mbedtls/mbedtls/library/psa_crypto_driver_wrappers.c
@@ -0,0 +1,1 @@
+/home/chiar/CSE121_Labs/esp-idf/components/mbedtls/mbedtls/library/psa_crypto_driver_wrappers.c
