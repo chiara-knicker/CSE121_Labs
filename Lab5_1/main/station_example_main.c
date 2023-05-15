2ae216f1c00375d476371f6682d0b9592aa39b9f
Chiara <120240559+chiara-knicker@users.noreply.github.com>
2023-05-15 01:17:46 -0700

    add functionality for https requests

--- Lab5_1/main/station_example_main.c
+++ Lab5_1/main/station_example_main.c
@@ -20,17 +20,38 @@
 #include "lwip/sys.h"
 
 // For HTTP requests
-//#include "protocol_examples_common.h"
-
 #include "lwip/sockets.h"
 #include "lwip/netdb.h"
 #include "lwip/dns.h"
 #include "sdkconfig.h"
 
+// For HTTPS requests
+#include <stdlib.h>
+#include <inttypes.h>
+#include <time.h>
+#include <sys/time.h>
+#include "esp_timer.h"
+#include "nvs.h"
+#include "esp_sntp.h"
+#include "esp_netif.h"
+
+#include "esp_tls.h"
+#if CONFIG_MBEDTLS_CERTIFICATE_BUNDLE
+#include "esp_crt_bundle.h"
+#endif
+#include "time_sync.h"
+
 /* Constants that aren't configurable in menuconfig */
-#define WEB_SERVER "example.com"
-#define WEB_PORT "80"
+#define WEB_SERVER "www.howsmyssl.com"
+#define WEB_PORT "443"
+// HTTPS
+#define WEB_URL "https://www.howsmyssl.com/a/check"
+#define SERVER_URL_MAX_SZ 256
+// HTTP
 #define WEB_PATH "/"
+
+/* Timer interval once every day (24 Hours) */
+#define TIME_PERIOD (86400000000ULL)
 
 /* The examples use WiFi configuration that you can set via project configuration menu
 
@@ -175,7 +196,8 @@ void wifi_init_sta(void)
     }
 }
 
-// For HTTP requests
+// ------------- For HTTP requests --------------
+
 static const char *REQUEST = "GET " WEB_PATH " HTTP/1.0\r\n"
     "Host: "WEB_SERVER":"WEB_PORT"\r\n"
     "User-Agent: esp-idf/1.0 esp32\r\n"
@@ -266,6 +288,234 @@ static void http_get_task(void *pvParameters)
     }
 }
 
+// ------------- For HTTPS requests --------------
+
+static const char HOWSMYSSL_REQUEST[] = "GET " WEB_URL " HTTP/1.1\r\n"
+                             "Host: "WEB_SERVER"\r\n"
+                             "User-Agent: esp-idf/1.0 esp32\r\n"
+                             "\r\n";
+
+#ifdef CONFIG_EXAMPLE_CLIENT_SESSION_TICKETS
+static const char LOCAL_SRV_REQUEST[] = "GET " CONFIG_EXAMPLE_LOCAL_SERVER_URL " HTTP/1.1\r\n"
+                             "Host: "WEB_SERVER"\r\n"
+                             "User-Agent: esp-idf/1.0 esp32\r\n"
+                             "\r\n";
+#endif
+
+/* Root cert for howsmyssl.com, taken from server_root_cert.pem
+
+   The PEM file was extracted from the output of this command:
+   openssl s_client -showcerts -connect www.howsmyssl.com:443 </dev/null
+
+   The CA root cert is the last cert given in the chain of certs.
+
+   To embed it in the app binary, the PEM file is named
+   in the component.mk COMPONENT_EMBED_TXTFILES variable.
+*/
+extern const uint8_t server_root_cert_pem_start[] asm("_binary_server_root_cert_pem_start");
+extern const uint8_t server_root_cert_pem_end[]   asm("_binary_server_root_cert_pem_end");
+
+extern const uint8_t local_server_cert_pem_start[] asm("_binary_local_server_cert_pem_start");
+extern const uint8_t local_server_cert_pem_end[]   asm("_binary_local_server_cert_pem_end");
+static const int server_supported_ciphersuites[] = {MBEDTLS_TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384, 0};
+static const int server_unsupported_ciphersuites[] = {MBEDTLS_TLS_ECDHE_RSA_WITH_ARIA_128_CBC_SHA256, 0};
+#ifdef CONFIG_EXAMPLE_CLIENT_SESSION_TICKETS
+static esp_tls_client_session_t *tls_client_session = NULL;
+static bool save_client_session = false;
+#endif
+
+static void https_get_request(esp_tls_cfg_t cfg, const char *WEB_SERVER_URL, const char *REQUEST)
+{
+    char buf[512];
+    int ret, len;
+
+    esp_tls_t *tls = esp_tls_init();
+    if (!tls) {
+        ESP_LOGE(TAG, "Failed to allocate esp_tls handle!");
+        goto exit;
+    }
+
+    if (esp_tls_conn_http_new_sync(WEB_SERVER_URL, &cfg, tls) == 1) {
+        ESP_LOGI(TAG, "Connection established...");
+    } else {
+        ESP_LOGE(TAG, "Connection failed...");
+        goto cleanup;
+    }
+
+#ifdef CONFIG_EXAMPLE_CLIENT_SESSION_TICKETS
+    /* The TLS session is successfully established, now saving the session ctx for reuse */
+    if (save_client_session) {
+        esp_tls_free_client_session(tls_client_session);
+        tls_client_session = esp_tls_get_client_session(tls);
+    }
+#endif
+
+    size_t written_bytes = 0;
+    do {
+        ret = esp_tls_conn_write(tls,
+                                 REQUEST + written_bytes,
+                                 strlen(REQUEST) - written_bytes);
+        if (ret >= 0) {
+            ESP_LOGI(TAG, "%d bytes written", ret);
+            written_bytes += ret;
+        } else if (ret != ESP_TLS_ERR_SSL_WANT_READ  && ret != ESP_TLS_ERR_SSL_WANT_WRITE) {
+            ESP_LOGE(TAG, "esp_tls_conn_write  returned: [0x%02X](%s)", ret, esp_err_to_name(ret));
+            goto cleanup;
+        }
+    } while (written_bytes < strlen(REQUEST));
+
+    ESP_LOGI(TAG, "Reading HTTP response...");
+    do {
+        len = sizeof(buf) - 1;
+        memset(buf, 0x00, sizeof(buf));
+        ret = esp_tls_conn_read(tls, (char *)buf, len);
+
+        if (ret == ESP_TLS_ERR_SSL_WANT_WRITE  || ret == ESP_TLS_ERR_SSL_WANT_READ) {
+            continue;
+        } else if (ret < 0) {
+            ESP_LOGE(TAG, "esp_tls_conn_read  returned [-0x%02X](%s)", -ret, esp_err_to_name(ret));
+            break;
+        } else if (ret == 0) {
+            ESP_LOGI(TAG, "connection closed");
+            break;
+        }
+
+        len = ret;
+        ESP_LOGD(TAG, "%d bytes read", len);
+        /* Print response directly to stdout as it is read */
+        for (int i = 0; i < len; i++) {
+            putchar(buf[i]);
+        }
+        putchar('\n'); // JSON output doesn't have a newline at end
+    } while (1);
+
+cleanup:
+    esp_tls_conn_destroy(tls);
+exit:
+    for (int countdown = 10; countdown >= 0; countdown--) {
+        ESP_LOGI(TAG, "%d...", countdown);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+#if CONFIG_MBEDTLS_CERTIFICATE_BUNDLE
+static void https_get_request_using_crt_bundle(void)
+{
+    ESP_LOGI(TAG, "https_request using crt bundle");
+    esp_tls_cfg_t cfg = {
+        .crt_bundle_attach = esp_crt_bundle_attach,
+    };
+    https_get_request(cfg, WEB_URL, HOWSMYSSL_REQUEST);
+}
+#endif // CONFIG_MBEDTLS_CERTIFICATE_BUNDLE
+
+static void https_get_request_using_cacert_buf(void)
+{
+    ESP_LOGI(TAG, "https_request using cacert_buf");
+    esp_tls_cfg_t cfg = {
+        .cacert_buf = (const unsigned char *) server_root_cert_pem_start,
+        .cacert_bytes = server_root_cert_pem_end - server_root_cert_pem_start,
+    };
+    https_get_request(cfg, WEB_URL, HOWSMYSSL_REQUEST);
+}
+
+static void https_get_request_using_specified_ciphersuites(void)
+{
+    ESP_LOGI(TAG, "https_request using server supported ciphersuites");
+    esp_tls_cfg_t cfg = {
+        .cacert_buf = (const unsigned char *) server_root_cert_pem_start,
+        .cacert_bytes = server_root_cert_pem_end - server_root_cert_pem_start,
+        .ciphersuites_list = server_supported_ciphersuites,
+    };
+
+    https_get_request(cfg, WEB_URL, HOWSMYSSL_REQUEST);
+
+    ESP_LOGI(TAG, "https_request using server unsupported ciphersuites");
+
+    cfg.ciphersuites_list = server_unsupported_ciphersuites;
+
+    https_get_request(cfg, WEB_URL, HOWSMYSSL_REQUEST);
+}
+
+static void https_get_request_using_global_ca_store(void)
+{
+    esp_err_t esp_ret = ESP_FAIL;
+    ESP_LOGI(TAG, "https_request using global ca_store");
+    esp_ret = esp_tls_set_global_ca_store(server_root_cert_pem_start, server_root_cert_pem_end - server_root_cert_pem_start);
+    if (esp_ret != ESP_OK) {
+        ESP_LOGE(TAG, "Error in setting the global ca store: [%02X] (%s),could not complete the https_request using global_ca_store", esp_ret, esp_err_to_name(esp_ret));
+        return;
+    }
+    esp_tls_cfg_t cfg = {
+        .use_global_ca_store = true,
+    };
+    https_get_request(cfg, WEB_URL, HOWSMYSSL_REQUEST);
+    esp_tls_free_global_ca_store();
+}
+
+#ifdef CONFIG_EXAMPLE_CLIENT_SESSION_TICKETS
+static void https_get_request_to_local_server(const char* url)
+{
+    ESP_LOGI(TAG, "https_request to local server");
+    esp_tls_cfg_t cfg = {
+        .cacert_buf = (const unsigned char *) local_server_cert_pem_start,
+        .cacert_bytes = local_server_cert_pem_end - local_server_cert_pem_start,
+        .skip_common_name = true,
+    };
+    save_client_session = true;
+    https_get_request(cfg, url, LOCAL_SRV_REQUEST);
+}
+
+static void https_get_request_using_already_saved_session(const char *url)
+{
+    ESP_LOGI(TAG, "https_request using saved client session");
+    esp_tls_cfg_t cfg = {
+        .client_session = tls_client_session,
+    };
+    https_get_request(cfg, url, LOCAL_SRV_REQUEST);
+    esp_tls_free_client_session(tls_client_session);
+    save_client_session = false;
+    tls_client_session = NULL;
+}
+#endif
+
+static void https_request_task(void *pvparameters)
+{
+    ESP_LOGI(TAG, "Start https_request example");
+
+#ifdef CONFIG_EXAMPLE_CLIENT_SESSION_TICKETS
+    char *server_url = NULL;
+#ifdef CONFIG_EXAMPLE_LOCAL_SERVER_URL_FROM_STDIN
+    char url_buf[SERVER_URL_MAX_SZ];
+    if (strcmp(CONFIG_EXAMPLE_LOCAL_SERVER_URL, "FROM_STDIN") == 0) {
+        example_configure_stdin_stdout();
+        fgets(url_buf, SERVER_URL_MAX_SZ, stdin);
+        int len = strlen(url_buf);
+        url_buf[len - 1] = '\0';
+        server_url = url_buf;
+    } else {
+        ESP_LOGE(TAG, "Configuration mismatch: invalid url for local server");
+        abort();
+    }
+    printf("\nServer URL obtained is %s\n", url_buf);
+#else
+    server_url = CONFIG_EXAMPLE_LOCAL_SERVER_URL;
+#endif /* CONFIG_EXAMPLE_LOCAL_SERVER_URL_FROM_STDIN */
+    https_get_request_to_local_server(server_url);
+    https_get_request_using_already_saved_session(server_url);
+#endif
+
+#if CONFIG_MBEDTLS_CERTIFICATE_BUNDLE
+    https_get_request_using_crt_bundle();
+#endif
+    ESP_LOGI(TAG, "Minimum free heap size: %" PRIu32 " bytes", esp_get_minimum_free_heap_size());
+    https_get_request_using_cacert_buf();
+    https_get_request_using_global_ca_store();
+    https_get_request_using_specified_ciphersuites();
+    ESP_LOGI(TAG, "Finish https_request example");
+    vTaskDelete(NULL);
+}
+
 void app_main(void)
 {
     //Initialize NVS
@@ -278,6 +528,10 @@ void app_main(void)
 
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
-   
+    
+    // HTTP request
     xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
+   
+    // HTTPS request
+    xTaskCreate(&https_request_task, "https_get_task", 8192, NULL, 5, NULL);
 }
