47ee321b6165de9a77b43ef6a21e735f421ae1b3
Chiara Knicker <120240559+chiara-knicker@users.noreply.github.com>
2023-07-04 22:05:12 +0100

    removed POST request code

--- Lab5_1/main/station_example_main.c
+++ Lab5_1/main/station_example_main.c
@@ -340,106 +340,6 @@ static void http_get_task(void *pvParameters)
     }
 }
 
-static const char *REQUEST_POST = "POST " WEB_PATH " HTTP/1.0\r\n"
-    "Host: chiara-raspberrypi:"WEB_PORT"\r\n"
-    "User-Agent: esp-idf/1.0 esp32\r\n"
-    "Content-Type: text/plain; charset=utf-8\r\n"
-    "Content-Length: %d\r\n"
-    "\r\n"
-    "%s";
-
-static const char *POST_DATA = "Hello World";
-
-static void http_post_task(void *pvParameters)
-{
-    const struct addrinfo hints = {
-        .ai_family = AF_INET,
-        .ai_socktype = SOCK_STREAM,
-    };
-    struct addrinfo *res;
-    struct in_addr *addr;
-    int s, r;
-    char recv_buf[64];
-
-    while(1) {
-        int err = getaddrinfo(WEB_SERVER, WEB_PORT, &hints, &res);
-
-        if(err != 0 || res == NULL) {
-            ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        /* Code to print the resolved IP.
-
-           Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code */
-        addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
-        ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
-
-        s = socket(res->ai_family, res->ai_socktype, 0);
-        if(s < 0) {
-            ESP_LOGE(TAG, "... Failed to allocate socket.");
-            freeaddrinfo(res);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        ESP_LOGI(TAG, "... allocated socket");
-
-        if(connect(s, res->ai_addr, res->ai_addrlen) != 0) {
-            ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
-            close(s);
-            freeaddrinfo(res);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        ESP_LOGI(TAG, "... connected");
-        freeaddrinfo(res);
-
-	/* Prepare HTTP POST request */
-	int post_data_len = strlen(POST_DATA);
-	char post_request[strlen(REQUEST_POST) + post_data_len];
-	sprintf(post_request, REQUEST_POST, post_data_len, POST_DATA);
-
-	if (write(s, post_request, strlen(post_request)) < 0) {
-            ESP_LOGE(TAG, "... socket send failed");
-            close(s);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        ESP_LOGI(TAG, "... socket send success");
-
-	struct timeval receiving_timeout;
-        receiving_timeout.tv_sec = 5;
-        receiving_timeout.tv_usec = 0;
-        if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
-                sizeof(receiving_timeout)) < 0) {
-            ESP_LOGE(TAG, "... failed to set socket receiving timeout");
-            close(s);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        ESP_LOGI(TAG, "... set socket receiving timeout success");
-
-        /* Read HTTP response */
-        do {
-            bzero(recv_buf, sizeof(recv_buf));
-            r = read(s, recv_buf, sizeof(recv_buf)-1);
-            for(int i = 0; i < r; i++) {
-                putchar(recv_buf[i]);
-            }
-        } while(r > 0);
-
-        ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d.", r, errno);
-        close(s);
-        for(int countdown = 10; countdown >= 0; countdown--) {
-            ESP_LOGI(TAG, "%d... ", countdown);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-        ESP_LOGI(TAG, "Starting again!");
-    }
-}
-
 // ------------- For HTTPS requests --------------
 
 static const char HOWSMYSSL_REQUEST[] = "GET " WEB_PATH " HTTP/1.1\r\n"
@@ -663,9 +563,6 @@ void app_main(void)
     
     // HTTP GET request
     //xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
-  
-    // HTTP POST request
-    //xTaskCreate(&http_post_task, "http_post_task", 4096, NULL, 5, NULL);
 
     // HTTPS request
     xTaskCreate(&https_request_task, "https_get_task", 8192, NULL, 5, NULL);
